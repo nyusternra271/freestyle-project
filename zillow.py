53cc3093e8ddf446a79cf3df040fb26ab13d2831
nyusternra271 <108551939+nyusternra271@users.noreply.github.com>
2022-08-07 14:32:16 -0400

    Updated several files
    - Created the skeleton for the Flask app
    - Fixed a typo in the requirements.txt file
    - Added the code from the Colab notebook the team has been working off of

--- zillow.py
+++ zillow.py
@@ -1,20 +1,50 @@
+from getpass import getpass
+from pandas import read_csv, read_json
+from plotly.express import line
 import requests
-from dotenv import load_dotenv
-
-import os
 import json
+import time
+import pandas
 
-load_dotenv()
 
-api_key = os.getenv("API_KEY", default="OOPS, please set the API key")
-print(api_key)
-request_url = "https://zillow56.p.rapidapi.com/search"
-query_string = { "location": "jamaica, ny"}
+# get keys
+rapid_api_key = getpass("Please input your API key: ")
+city = input("Please enter a city in: ")
+state = input("Please enter a state: ")
+search_str = city + ', ' + state
+print('Search string:', search_str)
+
+
+
+
+
+#c7fe818c2amshbc9b51c74fe5a8ep1983d3jsn15c748e613b8
+#https://zillow56.p.rapidapi.com/search
+
+url = "https://zillow56.p.rapidapi.com/search"
+
+querystring = {"location":search_str,
+               "home_type":"Houses",
+               "minPrice":"500000",
+               "maxPrice":"2000000"
+
+               
+               }
+
 headers = {
-    "X-RapidAPI-Key": api_key,
-    "X-RapidAPI-Host": "zillow56.p.rapidapi.com"
+	"X-RapidAPI-Key": rapid_api_key,
+	"X-RapidAPI-Host": "zillow56.p.rapidapi.com"
 }
 
-response = requests.request( "GET", request_url, headers=headers, params=query_string)
 
-print(type(response.text))
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+response_json = response.json()
+response_json
+
+# View Data
+prop_sale = pandas.json_normalize(data=response_json['results'])
+print('Num of rows:', len(prop_sale))
+print('Num of cols:', len(prop_sale.columns))
+prop_sale
+# 

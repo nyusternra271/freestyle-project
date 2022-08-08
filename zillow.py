5b1c5be2febaa8e9ac67dc010832b38a2673568f
Michael Noah <92764374+mnoah1@users.noreply.github.com>
2022-08-08 18:34:54 -0400

    Added Sendgrid

--- zillow.py
+++ zillow.py
@@ -1,12 +1,14 @@
 from getpass import getpass
 from pandas import read_csv, read_json, json_normalize
 from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 import requests
 import json
 import time
 #import pandas
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 
@@ -17,10 +19,6 @@ city = input("Please enter a city in: ")
 state = input("Please enter a state: ")
 search_str = city + ', ' + state
 print('Search string:', search_str)
-
-
-
-
 
 #c7fe818c2amshbc9b51c74fe5a8ep1983d3jsn15c748e613b8
 #https://zillow56.p.rapidapi.com/search
@@ -53,3 +51,17 @@ print('Num of rows:', len(prop_sale))
 print('Num of cols:', len(prop_sale.columns))
 print(prop_sale)
 # 
+
+message = Mail(
+    from_email='razi.ahmad1@gmail.com',
+    to_emails='razi.ahmad1@gmail.com',
+    subject='Zillow Property Update',
+    html_content='<strong>First row of data:</strong>' + prop_sale[0])
+try:
+    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e.message)
